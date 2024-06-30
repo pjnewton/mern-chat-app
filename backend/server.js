91db6148771b50f5b10060d610152ab00d34bd4a
PJ Newton <newtonoh3@gmail.com>
2024-06-30 14:04:23 -0700

    First part completed - Backend

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js';
+
+import connectToMongoDB from './db/connectToMongoDB.js';
+
+const app = express();
+const PORT = process.env.PORT || 5555;
+
+dotenv.config();
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server Running on port ${PORT}`);
+});
+
+// https://www.youtube.com/watch?v=HwCqsOis894
+// Starting FrontEnd: https://youtu.be/HwCqsOis894?si=rW7F4E7ywJN1UL3X&t=5895
