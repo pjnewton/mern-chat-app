11a93c5e3a971877f416584d16d1ee4478ef9932
PJ Newton <newtonoh3@gmail.com>
2024-07-06 16:40:26 -0700

    render deployment

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io('https://chat-app-project-mern.netlify.app', {
+      const socket = io('https://mern-chat-project.onrender.com', {
         query: {
           userId: authUser._id,
         },
