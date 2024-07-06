7bfb77eebe7c2cc0abe92774a4ff50f824609f97
PJ Newton <newtonoh3@gmail.com>
2024-07-06 15:45:59 -0700

    socket

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io('http://localhost:5001', {
+      const socket = io('https://chat-app-project-mern.netlify.app', {
         query: {
           userId: authUser._id,
         },
